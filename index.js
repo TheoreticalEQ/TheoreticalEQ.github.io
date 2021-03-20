66c218553ca1d6923aff9a21212533e739fef8f4
TheoreticalEQ <57850278+TheoreticalEQ@users.noreply.github.com>
2021-03-19 18:46:26 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 import TinyGesture from './TinyGesture.js';
 // Options object is optional. These are the defaults.
-
+const options = {
+  // Used to calculate the threshold to consider a movement a swipe. it is
+  // passed type of 'x' or 'y'.
+  threshold: (type, self) => Math.max(
+    25,
+    Math.floor(0.50 * (
+      type === 'x'
+        ? window.innerWidth || document.body.clientWidth
+        : window.innerHeight || document.body.clientHeight
+    ))
+  ),
+  // Minimum velocity the gesture must be moving when the gesture ends to be
+  // considered a swipe.
+  //velocityThreshold: 10,
+  // Used to calculate the distance threshold to ignore the gestures velocity
+  // and always consider it a swipe.
+//  disregardVelocityThreshold: (type, self) => Math.floor(
+//    0.5 * (
+  //    type === 'x'
+  //      ? self.element.clientWidth
+  //      : self.element.clientHeight
+  //  )
+//  ),
+  // Point at which the pointer moved too much to consider it a tap or longpress
+  // gesture.
+//  pressThreshold: 8,
+  // If true, swiping in a diagonal direction will fire both a horizontal and a
+  // vertical swipe.
+  // If false, whichever direction the pointer moved more will be the only swipe
+  // fired.
+//  diagonalSwipes: false,
+  // The degree limit to consider a swipe when diagonalSwipes is true.
+//  diagonalLimit: Math.tan(45 * 1.5 / 180 * Math.PI),
+  // Listen to mouse events in addition to touch events. (For desktop support.)
+//  mouseSupport: true
+};
 document.addEventListener('DOMContentLoaded', () => {
   const target = document.getElementById('fister');
-  const gesture = new TinyGesture(target);
-  let bgColor = null;
+  const gesture = new TinyGesture(target, options);
+  //let bgColor = null;
 
   gesture.on('panmove', () => {
   //console.log('panmove');
@@ -12,30 +47,30 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
     gesture.animationFrame = window.requestAnimationFrame(() => {
-      if (!gesture.swipingDirection.startsWith('pre-')) {
+      //if (!gesture.swipingDirection.startsWith('pre-')) {
         //target.style.backgroundColor = '#00AA00';
-      } else {
+      //} else {
         //target.style.backgroundColor = bgColor;
-      }
+      //}
       // const zDistance = -(Math.sqrt(Math.pow(gesture.touchMoveX, 2) + Math.pow(gesture.touchMoveY, 2)))+'px';
-      target.style.transition = 'background-color ease .3s';
+      //target.style.transition = 'background-color ease .3s';
       target.style.transform = 'perspective(1000px) translate3d('+gesture.touchMoveX+'px, '+gesture.touchMoveY+'px, 0)';
       //console.log('transform');
       window.requestAnimationFrame(() => {
-        target.style.transition = null;
+        //target.style.transition = null;
       });
       gesture.animationFrame = null;
     });
   });
 
   gesture.on('panend', () => {
-  console.log('panend');
-    window.cancelAnimationFrame(gesture.animationFrame);
-    gesture.animationFrame = null;
-    target.style.transition = null;
+  //console.log('panend');
+    //window.cancelAnimationFrame(gesture.animationFrame);
+    //gesture.animationFrame = null;
+    //target.style.transition = null;
     target.style.transform = null;
-    bgColor = null;
-    target.style.backgroundColor = bgColor;
+  //  bgColor = null;
+    //target.style.backgroundColor = bgColor;
   });
   /*
   gesture.on('swiperight', () => {
@@ -45,15 +80,20 @@ document.addEventListener('DOMContentLoaded', () => {
   */
 
   gesture.on('swipeleft', () => {
+    swipePunchWof()
+    /*
     console.log('swife left');
     showAngry();
     dropWolfHealth();
     blueTitle();
-    sleep(250).then(() => {
+    wolfGrowl()
+    sleep(100).then(() => {
       redTitle();
       showChill();
 
+
 });
+*/
     //target.style.transform = 'perspective(1000px) translate3d(-2000px, 0, 0)';
     setTimeout(() => target.style.transform = null, 1000);
   });
